2068c334ea8447a848a6f31ee3e4460d8be65f79
Pankaj Puri <pankajpuri016@gmail.com>
2023-03-03 20:45:14 +1300

    edit quote.js

--- index.js
+++ index.js
@@ -9,16 +9,19 @@ const config = require("config");
 const db = config.get("db");
 
 mongoose
-  .connect(db, {
+  .connect("mongodb://localhost:27017/turnerscars", {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   })
-  .then(() => console.log(`connected to the ${db}.....`))
+  .then(() =>
+    console.log(`connected to the mongodb://localhost:27017/turnerscars.....`)
+  )
   .catch((err) => console.error(`erros occured: ${err.message}`));
 
 app.use("/api/quote", quote);
 
 const port = process.env.PORT || 3000;
+
 const server = app.listen(port, () => {
   console.log(`listenting on port :${port}`);
 });
