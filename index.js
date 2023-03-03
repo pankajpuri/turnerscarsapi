d4a9196bf7b803fb3aad6702805dfd8aebcdfd32
Pankaj Puri <pankajpuri016@gmail.com>
2023-03-03 19:57:03 +1300

    edit quote.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const Joi = require("joi");
-//const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const quote = require("./routers/quotes");
 const express = require("express");
 const app = express();
 app.use(express.json());
+
+const config = require("config");
+const db = config.get("db");
+
+mongoose
+  .connect(db, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log(`connected to the ${db}.....`))
+  .catch((err) => console.error(`erros occured: ${err.message}`));
 
 app.use("/api/quote", quote);
 
